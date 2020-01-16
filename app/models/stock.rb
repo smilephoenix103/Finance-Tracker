ba196e2b64f0cd99728ff36f7509744b1ba399ed
smilephoenix103 <marugotomoaki@gmail.com>
2020-01-16 11:31:32 +0000

    Save credentials securely for IEX

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,2 +1,10 @@
 class Stock < ApplicationRecord
+  def self.new_lookup(ticker)
+    client = IEX::Api::Client.new(
+      publishable_token: Rails.application.credentials.iex[:publishable_key],
+      endpoint: 'https://sandbox.iexapis.com/v1'
+    )
+
+    client.price(ticker)
+  end
 end
