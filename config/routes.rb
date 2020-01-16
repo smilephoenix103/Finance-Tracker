148a15c2b7f5594d180b34af04efe3285df79c86
smilephoenix103 <marugotomoaki@gmail.com>
2020-01-16 11:37:41 +0000

    Create Users controller with my_portfolio action

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
 
   devise_for :users
-  # get 'welcome#index'
 
   root 'welcome#index'
+
+  get 'my_portfolio', to: 'users#my_portfolio'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
