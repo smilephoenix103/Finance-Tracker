3b02493676dddfc7059a6f2e7a59446bcdb8d5cd
smilephoenix103 <marugotomoaki@gmail.com>
2020-01-21 10:23:48 +0000

    Add portfoliio stock removal.
    Make all buttons small.

--- app/controllers/user_stocks_controller.rb
+++ app/controllers/user_stocks_controller.rb
@@ -9,7 +9,15 @@ class UserStocksController < ApplicationController
     end
 
     UserStock.create(user: current_user, stock: stock)
-    flash[:notice] = "#{stock.name} was added to your portfolio"
+    flash[:notice] = "#{stock.name} (#{stock.ticker}) was added to your portfolio"
+    redirect_to my_portfolio_path
+  end
+
+  def destroy
+    stock = Stock.find params[:id]
+
+    UserStock.where(user_id: current_user.id, stock_id: stock.id).first.destroy
+    flash[:notice] = "#{stock.name} (#{stock.ticker}) was removed from your portfolio"
     redirect_to my_portfolio_path
   end
 end
