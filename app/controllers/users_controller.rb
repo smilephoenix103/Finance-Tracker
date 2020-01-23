ca9a89d5624306400ab12ef3eeac30a2b52c6dc7
smilephoenix103 <marugotomoaki@gmail.com>
2020-01-23 10:17:10 +0000

    Add friendships and listing page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,12 @@
 class UsersController < ApplicationController
   def edit
-
   end
 
   def my_portfolio
     @tracked_stocks = current_user.stocks
+  end
+
+  def my_friends
+    @friends = current_user.friends
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
 Rails.application.routes.draw do
 
   resources :user_stocks, only: [:create, :destroy]
+  resources :friends, only: [:create, :destroy]
+
   devise_for :users
 
   root 'welcome#index'
 
   get 'my_portfolio', to: 'users#my_portfolio'
+  get 'my_friends', to: 'users#my_friends'
   get 'search_stock', to: 'stocks#search'
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
